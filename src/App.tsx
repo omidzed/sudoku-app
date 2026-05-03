08f46b0174dc07a53c45614b5476dc6cd3b0a5f2
Omid <omid-develops@gmail.com>
2026-05-02 23:20:34 -0700

    added UI for cell selection

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import type { Board } from "./types";
 
 function App() {
   const [board, setBoard] = useState<Board | null>(null);
+  const [initialBoard, setInitialBoard] = useState<Board | null>(null);
+  const [selectedCell, setSelectedCell] = useState<{
+    row: number;
+    col: number;
+  } | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -16,6 +21,7 @@ function App() {
         const data = await fetchSudoku();
         console.log("API DATA:", data);
         setBoard(data.puzzle);
+        setInitialBoard(data.puzzle);
       } catch (err) {
         console.error(err);
         setError("Failed to load Sudoku!");
@@ -46,9 +52,10 @@ function App() {
     }
     setBoard(newBoard);
   };
+  console.log("selectedCell:", selectedCell);
 
   return (
-    <section className="flex flex-col items-center justify-center h-screen bg-linear-to-r from-blue-400 to-purple-600">
+    <section className="flex flex-col items-center justify-center min-h-dvh w-full bg-linear-to-r from-blue-400 to-purple-600">
       <h1 className="text-6xl text-white p-6">SUDOKU</h1>
       {loading && <p className="text-white mt-4">Loading...</p>}
       {error && <p className="text-red-400 mt-4">{error}</p>}
@@ -57,12 +64,17 @@ function App() {
         <div className="max-w-fit grid grid-cols-9">
           {board.map((row, rowIndex) =>
             row.map((cell, colIndex) => {
-              let borders = "";
-
-              if (rowIndex % 3 === 0) borders += " border-t-4";
-              if (colIndex % 3 === 0) borders += " border-l-4";
-              if (rowIndex === 8) borders += " border-b-4";
-              if (colIndex === 8) borders += " border-r-4";
+              let styling = "";
+                
+              if (rowIndex % 3 === 0) styling += " border-t-4";
+              if (colIndex % 3 === 0) styling += " border-l-4";
+              if (rowIndex === 8) styling += " border-b-4";
+              if (colIndex === 8) styling += " border-r-4";
+              if (
+                selectedCell?.row === rowIndex &&
+                selectedCell?.col === colIndex
+              )
+                styling += " bg-yellow-400";
 
               const boxRow = Math.floor(rowIndex / 3);
               const boxCol = Math.floor(colIndex / 3);
@@ -71,15 +83,21 @@ function App() {
               return (
                 <div
                   key={`${rowIndex}-${colIndex}`}
+                  onClick={() =>
+                    setSelectedCell({ row: rowIndex, col: colIndex })
+                  }
                   className={`w-12 h-12 flex items-center justify-center border border-blue-900  ${
-                    isLightBox ? "bg-white text-blue-800" : "bg-blue-400 text-white"
-                  }${borders}`}
+                    isLightBox
+                      ? "bg-white text-blue-800"
+                      : "bg-blue-400 text-white"
+                  } 
+                  ${styling}`}
                 >
                   <input
                     value={cell ?? ""}
                     type="text"
                     maxLength={1}
-                    disabled={cell !== null}
+                    disabled={initialBoard?.[rowIndex][colIndex] !== null}
                     onChange={(e) =>
                       handleInputEdit(e.target.value, rowIndex, colIndex)
                     }
